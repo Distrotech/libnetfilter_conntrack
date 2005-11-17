ade771be804b64a5d5a5aede5d1a6d4fe6e6a43b
/C=DE/ST=Berlin/L=Berlin/O=Netfilter Project/OU=Development/CN=laforge/emailAddress=laforge@netfilter.org </C=DE/ST=Berlin/L=Berlin/O=Netfilter Project/OU=Development/CN=laforge/emailAddress=laforge@netfilter.org>
2005-11-17 13:07:08 +0000

    add KERNEL_DIR option to autogen.sh for all new libraries (Krzysztof Oledzki)

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ include ()
     # the headers that we need from the lastest kernel version at autogen
     # stage.
 
-    INCLUDEDIR=/lib/modules/`uname -r`/build/include/linux
+    INCLUDEDIR=${KERNEL_DIR:-/lib/modules/`uname -r`/build}/include/linux
     if [ -f $INCLUDEDIR/netfilter/nfnetlink_conntrack.h ]
     then
     	TARGET=include/libnetfilter_conntrack/linux_nfnetlink_conntrack.h
@@ -17,7 +17,7 @@ include ()
 	sed 's/linux\/netfilter\/nfnetlink.h/libnfnetlink\/linux_nfnetlink.h/g' $TARGET > $TEMP
 	mv $TEMP $TARGET
     else
-    	echo "can't find nfnetlink_conntrack.h kernel file"
+    	echo "can't find nfnetlink_conntrack.h kernel file in $INCLUDEDIR"
     	exit 1
     fi
 }
