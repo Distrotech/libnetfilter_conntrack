0afc25526d9803d857217bc0b9a9c87e6efb5e77
/C=DE/ST=Berlin/L=Berlin/O=Netfilter Project/OU=Development/CN=pablo/emailAddress=pablo@netfilter.org </C=DE/ST=Berlin/L=Berlin/O=Netfilter Project/OU=Development/CN=pablo/emailAddress=pablo@netfilter.org>
2006-01-15 03:08:46 +0000

    o Add missing layer-3 protocol flags for the expectation tuple o Update copyright date

--- src/libnetfilter_conntrack.c
+++ src/libnetfilter_conntrack.c
@@ -1,6 +1,6 @@
 /*
- * (C) 2005 by Pablo Neira Ayuso <pablo@netfilter.org>
- *             Harald Welte <laforge@netfilter.org>
+ * (C) 2005-2006 by Pablo Neira Ayuso <pablo@netfilter.org>
+ *                  Harald Welte <laforge@netfilter.org>
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
