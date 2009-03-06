76e6042107de23790f0532e3bf3c396cba27e5aa
Pablo Neira Ayuso <pablo@netfilter.org>
2009-03-06 17:22:01 +0100

    include: deprecate several header files with obsolete enums
    This patch deprecates several header files that contain enums that
    were define in the very old libnetfilter_conntrack API.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/libnetfilter_conntrack/libnetfilter_conntrack_icmp.h
+++ include/libnetfilter_conntrack/libnetfilter_conntrack_icmp.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2005 by Pablo Neira Ayuso <pablo@eurodev.net>
+ * (C) 2005 by Pablo Neira Ayuso <pablo@netfilter.org>
  *
  * This software may be used and distributed according to the terms
  * of the GNU General Public License, incorporated herein by reference.
@@ -8,23 +8,6 @@
 #ifndef _LIBNETFILTER_CONNTRACK_ICMP_H_
 #define _LIBNETFILTER_CONNTRACK_ICMP_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-enum icmp_flags {
-	ICMP_TYPE_BIT = 0,
-	ICMP_TYPE = (1 << ICMP_TYPE_BIT),
-
-	ICMP_CODE_BIT = 1,
-	ICMP_CODE = (1 << ICMP_CODE_BIT),
-
-	ICMP_ID_BIT = 2,
-	ICMP_ID = (1 << ICMP_ID_BIT)
-};
-
-#ifdef __cplusplus
-}
-#endif
+#warning "Please, remove libnetfilter_conntrack_icmp.h from your includes!"
 
 #endif
--- include/libnetfilter_conntrack/libnetfilter_conntrack_tcp.h
+++ include/libnetfilter_conntrack/libnetfilter_conntrack_tcp.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2005 by Pablo Neira Ayuso <pablo@eurodev.net>
+ * (C) 2005 by Pablo Neira Ayuso <pablo@netfilter.org>
  *
  * This software may be used and distributed according to the terms
  * of the GNU General Public License, incorporated herein by reference.
@@ -25,35 +25,6 @@ enum tcp_state {
 	TCP_CONNTRACK_LISTEN,
 	TCP_CONNTRACK_MAX,
 	TCP_CONNTRACK_IGNORE
-};
-
-enum tcp_flags {
-	TCP_ORIG_SPORT_BIT = 0,
-	TCP_ORIG_SPORT = (1 << TCP_ORIG_SPORT_BIT),
-
-	TCP_ORIG_DPORT_BIT = 1,
-	TCP_ORIG_DPORT = (1 << TCP_ORIG_DPORT_BIT),
-
-	TCP_REPL_SPORT_BIT = 2,
-	TCP_REPL_SPORT = (1 << TCP_REPL_SPORT_BIT),
-
-	TCP_REPL_DPORT_BIT = 3,
-	TCP_REPL_DPORT = (1 << TCP_REPL_DPORT_BIT),
-
-	TCP_MASK_SPORT_BIT = 4,
-	TCP_MASK_SPORT = (1 << TCP_MASK_SPORT_BIT),
-
-	TCP_MASK_DPORT_BIT = 5,
-	TCP_MASK_DPORT = (1 << TCP_MASK_DPORT_BIT),
-
-	TCP_STATE_BIT = 6,
-	TCP_STATE = (1 << TCP_STATE_BIT),
-
-	TCP_EXPTUPLE_SPORT_BIT = 7,
-	TCP_EXPTUPLE_SPORT = (1 << TCP_EXPTUPLE_SPORT_BIT),
-
-	TCP_EXPTUPLE_DPORT_BIT = 8,
-	TCP_EXPTUPLE_DPORT = (1 << TCP_EXPTUPLE_DPORT_BIT)
 };
 
 #ifdef __cplusplus
